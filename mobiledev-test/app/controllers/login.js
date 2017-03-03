e260c90079927f351d24384cbabd2abee08e84bd
Chris H <chri163v@edu.eal.dk>
2017-03-03 21:50:15 +0100

    uploaded a backup 3-3-17
    Still messing with image-linking, how does this shit work

---
+++ mobiledev-test/app/controllers/login.js
@@ -0,0 +1,22 @@
+// Arguments passed into this controller can be accessed via the `$.args` object directly or:
+var args = $.args;
+/*
+function checkLogin()
+{
+	var uText = $.username.value;
+	var pText = $.password.value;
+	var correctUser = "cdh";
+	var correctPass ="0000";
+	var
+	
+	if(uText === correctUser && pText === correctPass); // maybe add in lowercase-check?
+	{
+		loginSuccesful();
+	}
+}
+
+function loginSuccesful() 
+{
+	
+}
+*/
